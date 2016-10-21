e7f700b60dc5b1651c6b975e855a0b81c4dcb66c
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-21 16:12:33 +0100

    adds solution to challenge 16. Tags can now be searched/printed by accessing /tags/<desired_tag>

--- app/app.rb
+++ app/app.rb
@@ -32,6 +32,20 @@ class BookmarkManager < Sinatra::Base
     erb :'links/create_link'
   end
 
+    # xyz here is a placeholder/variable
+  get '/tags/:xyz' do
+    # @links because that's the syntax the /links view is expecting when it prints
+    # Tag.first info can be found at http://datamapper.org/docs/find.html
+    @links = Tag.first(:tag_name_in_database => params[:xyz]).links
+    erb :'links/index'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
+end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params['name'] is 'foo' or 'bar'
+  "Hello #{params['name']}!"
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -12,3 +12,14 @@ end
 def submit
   click_button 'Submit'
 end
+
+
+1 zoo  = Zoo.get(1)                     # get the zoo with primary key of 1.
+2 zoo  = Zoo.get!(1)                    # Or get! if you want an ObjectNotFoundError on failure
+3 zoo  = Zoo.get('DFW')                 # wow, support for natural primary keys
+4 zoo  = Zoo.get('Metro', 'DFW')        # more wow, composite key look-up
+5 zoo  = Zoo.first(:name => 'Metro')    # first matching record with the name 'Metro'
+6 zoo  = Zoo.last(:name => 'Metro')     # last matching record with the name 'Metro'
+7 zoos = Zoo.all                        # all zoos
+8 zoos = Zoo.all(:open => true)         # all zoos that are open
+9 zoos = Zoo.all(:opened_on => (s..e))  # all zoos that opened on a date in the date-range
